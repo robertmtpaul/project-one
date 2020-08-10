13c64ffde7718236ad8c8bf342be3111042f8ea7
robertmtpaul <rob@glo.id.au>
2020-08-10 13:43:01 +1000

    Added pages controller and routes to set root page to the pages#welcome page. Added erb files for both welcome and home pages. New_user_path not working and needed to be commented out temporarily. Added checks to see if user is logged in to pages_controller.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+    before_action :check_if_logged_in, only: [ :home ]
+
+    def welcome
+    end
+
+    def home
+        check_if_logged_in
+    end
+
+    private
+
+end # Pages_controller
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'pages#welcome'
+  get '/home' => 'pages#home'
+  # Session routes;
+  get '/login' => 'session#new'
+
+  
   get 'users/new'
   get 'users/create'
   get 'users/index'
