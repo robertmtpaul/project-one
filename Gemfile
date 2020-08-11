482cba9d3a6b68bdc34e40d3302e49942e128dfd
robertmtpaul <rob@glo.id.au>
2020-08-11 16:24:12 +1000

    Working index and show page.

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pry-rails'
 gem 'pry-byebug'
+
+gem 'cloudinary'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new # b/c the form_form helper needs @user.
   end
 
   def create
+
     @user = User.create user_params
 
+    # Handle upload, if file was uploaded
+    if params[:file].present?
+      # actually forward uploaded file on to Cloudinary server
+      response = Cloudinary::Uploader.upload params[:file]
+      @user.image = response['public_id']      
+      @user.save
+    end
+
+    # @user.user_id = @current_user.id
+    #@user.save
+    
     if @user.persisted?
       #check whether the above create successful
       session[:user_id] = @user.id  #log in the newly created account
@@ -16,11 +32,9 @@ class UsersController < ApplicationController
     end
   end #create
 
-  def index
-  end
-
   def show
-  end
+    @user = User.find params[:id]
+  end 
 
   def edit
   end
